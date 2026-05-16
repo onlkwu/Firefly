23d8ec78ee5ed6e9b31a3d86408304ec48ec5789
onlkwu <267063441+onlkwu@users.noreply.github.com>
2026-05-17 02:32:12 +0800

    我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "blog.onlkwu-blog.ccwu.cc",
 
 	// 站点描述
 	description:
